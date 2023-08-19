1d67320b7959d7e48ab5e0d0432e3c173a21d412
Andrey Vorontsov <reygofor@gmail.com>
2023-08-20 01:03:13 +0300

    oop.py1.02: add 1.1

---
+++ oop/py1.02.py
@@ -0,0 +1,44 @@
+""" 1.1
+Classes for editing modes in emacs and variety of major and minor instances of modes.
+- emacs
+    - class
+        - major editing mode
+            - instance
+                - Lisp mode
+                - Python mode
+                - Haskell mode
+    - class
+        - minor editing modes
+            - instance
+                - Eldoc mode
+                - Flyspell mode
+                - Auto-fill mode
+
+Class sidebar icons is a template for variety of instances of icons and 
+class bottom panel is a template for variety of instances of bottom panels
+- vs code
+    - class
+        - sidebar icons
+            - instance
+                - search
+                - debug
+                - test
+    - class
+        - bottom panel
+            - instance
+                - input data
+                - terminal
+                - debug console
+
+Class tab is a template for variety of instances of tabs and 
+class bookmark is a template for variety of instances of bookmarks
+- brave browser
+    - class
+        - tab
+            - instance
+                - new tab (url)
+    - class
+        - bookmark
+            - instance
+                - new bookmark (url)
+"""
