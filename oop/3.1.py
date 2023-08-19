5b26de895459af9ea802db50acbfc8a2f3285bdf
Andrey Vorontsov <reygofor@gmail.com>
2023-08-19 12:31:26 +0300

    oop.3.1: add task 2

--- oop/3.1.py
+++ oop/3.1.py
@@ -24,3 +24,33 @@ print("Task 1 ----------------------")
 c1.get_info()
 c1.get_sound()
 c1.get_sound()
+
+
+# Task 2
+
+
+class Book:
+    def __init__(self, title: str, author: str, pages: int) -> None:
+        self.title: str = title
+        self.author: str = author
+        self.pages: int = pages
+
+    def get_page(self, page: int) -> None:
+        if page > 0 and page <= self.pages:
+            print(f"Page {page} is opened")
+        else:
+            print(f"Page {page} does not exist")
+
+    def get_info(self) -> None:
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Pages: {self.pages}")
+
+
+b1: Book = Book(title="SICP", author="Wizards", pages=600)
+
+print("\nTask 2 ----------------------")
+b1.get_info()
+b1.get_page(page=0)
+b1.get_page(page=600)
+b1.get_page(page=601)
