5835e05ac87cb1506306ef2db44016950c8245bb
Andrey Vorontsov <reygofor@gmail.com>
2023-08-31 17:01:16 +0300

    oop/py.1.13: add unittest for quick sort with logging

---
+++ oop/py.1.13/readme.md
@@ -0,0 +1,17 @@
+# Testing/Unittest
+
+## What to test
+
+Quick Sort in: [py_1_13](py_1_13.py)
+
+## Testing file
+
+Here i have a test file: [test_quick_sort.py](test_quick_sort.py)
+
+## Logs
+
+Here i have a log file for unittest: [log.log](log.log)
+
+## Console output
+
+![console](https://i.imgur.com/cEIOgCq.png)
---
+++ oop/py.1.13/test_quick_sort.py
@@ -0,0 +1,63 @@
+# type: ignore
+import logging
+import unittest
+from random import randint
+
+from py_1_13 import quick_sort
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="log.log",
+    format=" %(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    encoding="utf-8",
+)
+
+logger = logging.getLogger(__name__)
+
+
+class TestQuickSort(unittest.TestCase):
+    def test_regression(self):
+        lst = [8, -1, 2, 7, 0, 5, 9, 3, 6, 4, 10, 1]
+        regr = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        logging.info("Running regression test")
+        try:
+            self.assertEqual(quick_sort(lst), regr)
+            logging.info("Regression test passed")
+        except AssertionError:
+            logging.error("Regression test failed")
+            self.assertEqual(quick_sort(lst), regr)
+
+    def test_random(self):
+        lst = [randint(-100, 100) for _ in range(10)]
+        logging.info("Running random test")
+        try:
+            self.assertEqual(quick_sort(lst), sorted(lst))
+            logging.info("Random test passed")
+        except AssertionError:
+            logging.error("Random test failed")
+            self.assertEqual(quick_sort(lst), sorted(lst))
+
+    def test_null(self):
+        logging.info("Running null test")
+        try:
+            self.assertEqual(quick_sort([0, 0, 0]), [0, 0, 0])
+            logging.info("Null test passed")
+        except AssertionError:
+            logging.error("Null test failed")
+            self.assertEqual(quick_sort([0, 0, 0]), [0, 0, 0])
+
+    def test_boundary(self):
+        arr = [1000000000000, -1000000000000, 0, -1, 1]
+        result = quick_sort(arr)
+        logging.info("Running boundary test")
+        try:
+            self.assertEqual(result, [-1000000000000, -1, 0, 1, 1000000000000])
+            logging.info("Boundary test passed")
+        except AssertionError:
+            logging.error("Boundary test failed")
+            self.assertEqual(result, [-1000000000000, -1, 0, 1, 1000000000000])
+
+
+if __name__ == "__main__":
+    unittest.main()
