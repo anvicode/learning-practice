154753e51fa851ed2b538e43fb48e0dfacea3a5e
Andrey Vorontsov <reygofor@gmail.com>
2023-09-06 17:48:36 +0300

    oop/py.1.16: add readme

--- oop/py.1.16/main.py
+++ oop/py.1.16/main.py
@@ -4,10 +4,10 @@ from zipfile import ZipFile
 
 
 def add_to_zip(zip_name, ext):
-    # if not zip_name.endswith(".zip"):
-    #     zip_name += ".zip"
-    # if not ext.startswith("."):
-    #     ext = "." + ext
+    if not zip_name.endswith(".zip"):
+        zip_name += ".zip"
+    if not ext.startswith("."):
+        ext = "." + ext
     with ZipFile(zip_name, "w") as zip_file:
         for root, dirs, files in os.walk("."):
             # only for current directory
---
+++ oop/py.1.16/readme.md
@@ -0,0 +1,9 @@
+# Working with archives
+
+## [main.py](main.py)
+
+Contains the `add_to_zip()` function. It adds files with certain extensions to archive with given name.
+
+## [test_main.py](test_main.py)
+
+Tests with [logging](logs.log). My tests are poor so far. But I started to understand a little how to use them. Still i need to dig into testing more.
